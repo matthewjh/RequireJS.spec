c30c654b5a6363880102f79058c93c20e9859880
Matthew Hill <Matthew.Hill4@bskyb.com>
2015-03-23 21:31:50 +0000

    adds almond

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,27 @@
-var gulp = require('gulp'),
-    jshint = require('gulp-jshint'),
-    rjs = require('gulp-requirejs');
+var gulp = require('gulp');
 
 gulp.task('default', function () {
   // place code for your default task here
 });
 
 gulp.task('lint', function () {
+  var jshint = require('gulp-jshint');
+
   return gulp.src('src/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'));
 });
 
 gulp.task('rjs-build', function () {
+  var rjs = require('gulp-requirejs');
+
   rjs({
     baseUrl: 'src/',
+    name: '../bower_components/almond/almond',
     mainConfigFile: 'src/requirejs-config.js',
     out: 'built.js',
     include: ['test'],
+    wrap: true,
     insertRequire: ['test']
   })
   .pipe(gulp.dest('./'));
