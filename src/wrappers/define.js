0eb7ae2f17cca08353375343162eb16a6d74c1c3
Matthew Hill <Matthew.Hill4@bskyb.com>
2015-03-26 18:11:30 +0000

    updates comments

--- src/wrappers/define.js
+++ src/wrappers/define.js
@@ -5,19 +5,19 @@ define([
   var defineWrapper;
 
   defineWrapper = function (id, dependencies, factory) {
-    // When define is called with the 'id' and 'dependencies' omitted
+    // When called with 'id' and 'dependencies' omitted
     if (id.constructor === Function) {
       factory = id;
       id = null;
       dependencies = null;
-    } // When define is called with the 'id' omitted
+    } // When called with 'id' omitted
       else if (id.constructor === Array) {
       factory = dependencies;
       dependencies = id;
       id = null;
     }
 
-    // When define is called with 'dependencies' omitted
+    // When called with 'dependencies' omitted
     if (dependencies && dependencies.constructor && dependencies.constructor === Function) {
       factory = dependencies;
       dependencies = null;
